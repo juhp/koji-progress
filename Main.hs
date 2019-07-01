412c06157891b0272743456133006c0a25984bdc
Jens Petersen <petersen@redhat.com>
2019-07-01 03:17:16 +0000

    import <$>, <*> for old ghc7

--- Main.hs
+++ Main.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+#if (defined(MIN_VERSION_base) && MIN_VERSION_base(4,8,0))
+#else
+import Control.Applicative ((<$>), (<*>))
+#endif
 import Control.Monad (unless)
 
 import Network.HTTP.Client (Manager)
